91a969fbc1854549ffbf8dd2b606443bd3eab528
Keksike <cihan.m.bebek@gmail.com>
2016-02-04 09:16:44 +0200

    karma.conf.js was missing parse.min.js from files

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -35,6 +35,7 @@ module.exports = function(config) {
       'bower_components/angular-touch/angular-touch.js',
       'bower_components/angular-bootstrap/ui-bootstrap-tpls.js',
       'bower_components/angular-recaptcha/release/angular-recaptcha.js',
+      'bower_components/parse/parse.min.js',
       'bower_components/ng-bs-animated-button/ng-bs-animated-button.js',
       'bower_components/underscore/underscore.js',
       'bower_components/angular-mocks/angular-mocks.js',
